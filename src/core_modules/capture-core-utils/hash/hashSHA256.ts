308bde4d1907c98cb16569802b3c3d87f23b0371
nshandra <34254522+nshandra@users.noreply.github.com>
2026-01-25 21:14:53 +0100

    fix: solve "The application could not be loaded." issue caused by secure context (http non localhost sites). Additionally allows to use the app in private mode.

--- src/core_modules/capture-core-utils/hash/hashSHA256.ts
+++ src/core_modules/capture-core-utils/hash/hashSHA256.ts
@@ -1,6 +1,12 @@
+import { sha256 } from 'js-sha256';
+
 export const hashSHA256 = async (input: string): Promise<string> => {
-    const inputUint8 = new TextEncoder().encode(input);
-    const hashBuffer = await window.crypto.subtle.digest('SHA-256', inputUint8);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+    try {
+        const inputUint8 = new TextEncoder().encode(input);
+        const hashBuffer = await window.crypto.subtle.digest('SHA-256', inputUint8);
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+    } catch (error) {
+        return sha256(input);
+    }
 };
