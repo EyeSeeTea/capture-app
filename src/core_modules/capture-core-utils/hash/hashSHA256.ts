3bc7d5833e0808096fca451c1309a8bd176f8679
nshandra <34254522+nshandra@users.noreply.github.com>
2026-01-26 11:28:09 +0100

    fix: switch try/catch to conditional to avoid masking unrelated problems

--- src/core_modules/capture-core-utils/hash/hashSHA256.ts
+++ src/core_modules/capture-core-utils/hash/hashSHA256.ts
@@ -1,12 +1,13 @@
 import { sha256 } from 'js-sha256';
 
 export const hashSHA256 = async (input: string): Promise<string> => {
-    try {
-        const inputUint8 = new TextEncoder().encode(input);
-        const hashBuffer = await window.crypto.subtle.digest('SHA-256', inputUint8);
-        const hashArray = Array.from(new Uint8Array(hashBuffer));
-        return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
-    } catch (error) {
+    if (!window.crypto?.subtle) {
+        console.warn('SubtleCrypto API not available, falling back to js-sha256.');
         return sha256(input);
     }
+
+    const inputUint8 = new TextEncoder().encode(input);
+    const hashBuffer = await window.crypto.subtle.digest('SHA-256', inputUint8);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
 };
